f164be71bbf8fb5c45addcb37d0de98a73d4d572
bukwon <148305892+bukwon@users.noreply.github.com>
2023-12-18 23:16:08 +0900

    [Bronze III] Title: 약수 구하기, Time: 124 ms, Memory: 14216 KB -BaekjoonHub

---
+++ 백준/Bronze/2501. 약수 구하기/약수 구하기.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());   // 6
+        int k = Integer.parseInt(st.nextToken());   // 3
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                k--;
+            }
+            //k번째 약수이면 i 출력
+            if (k == 0) {
+                System.out.print(i);
+                return;
+            }
+        }
+        //k번째 약수가 없으면 0 출력
+        System.out.print(0);
+    } // k= 6
+}
