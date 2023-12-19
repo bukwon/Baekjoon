728a745c2d9f4c21f34b6e1794407ef98d664ac6
bukwon <148305892+bukwon@users.noreply.github.com>
2023-12-19 15:47:26 +0900

    [Bronze III] Title: 대지, Time: 328 ms, Memory: 27632 KB -BaekjoonHub

--- 백준/Bronze/9063. 대지/대지.java
+++ 백준/Bronze/9063. 대지/대지.java
@@ -5,10 +5,10 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        
-        int max_x = Integer.MIN_VALUE; 
+
+        int max_x = Integer.MIN_VALUE;
         int max_y = Integer.MIN_VALUE;
-        int min_x = Integer.MAX_VALUE; 
+        int min_x = Integer.MAX_VALUE;
         int min_y = Integer.MAX_VALUE;
 
         int N = Integer.parseInt(bf.readLine());
@@ -17,15 +17,13 @@ public class Main {
             StringTokenizer st = new StringTokenizer(bf.readLine());
             int x = Integer.parseInt(st.nextToken());
             int y = Integer.parseInt(st.nextToken());
-
-            // 조건식을 수정하여 최소/최대 값을 올바르게 갱신하도록 수정
-            max_x = Math.max(max_x, x);
-            max_y = Math.max(max_y, y);
-            min_x = Math.min(min_x, x);
-            min_y = Math.min(min_y, y);
+            
+            if (x > max_x) max_x=x;
+            if (y > max_y) max_y=y;
+            if (x < min_x) min_x=x;
+            if (y < min_y) min_y=y;
         }
 
-        // N이 1일 때의 예외 처리를 bw.write로 변경
         if (N == 1) {
             bw.write("0\n");
         } else {
