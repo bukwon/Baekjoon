4bd620569702a6514dda4465ee504119398d3803
bukwon <148305892+bukwon@users.noreply.github.com>
2023-12-20 13:09:19 +0900

    [Bronze III] Title: 최댓값, Time: 248 ms, Memory: 18700 KB -BaekjoonHub

---
+++ 백준/Bronze/2566. 최댓값/최댓값.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[][] arr = new int[10][10];
+        int X = 0; //행
+        int Y = 0;//열
+        int max = 0;
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if(arr[i][j] > max) {
+                    max = arr[i][j]; X = i; Y = j;}
+            }
+        }
+        System.out.println(max);
+        System.out.println((X+1) + " " + (Y+1));
+    }
+}
