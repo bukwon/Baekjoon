cdb509492c5dbb9d6ac43e25dc92941de93b877e
bukwon <148305892+bukwon@users.noreply.github.com>
2023-12-20 16:21:24 +0900

    [Bronze III] Title: 최댓값, Time: 216 ms, Memory: 17620 KB -BaekjoonHub

---
+++ 백준/Bronze/2562. 최댓값/최댓값.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] num = new int[9];
+        int max = num[0];
+        int maxNum = 0;
+        for (int i = 0; i < num.length; i++) {
+            num[i] = sc.nextInt();
+            if (num[i] > max) {
+                max = num[i];
+                maxNum = i + 1;
+            }
+        }
+        System.out.println(max);
+        System.out.println(maxNum);
+    }
+}
