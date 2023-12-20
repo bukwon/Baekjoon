2f56d2f3f272dd83997d51a1c700bd8a64b36102
bukwon <148305892+bukwon@users.noreply.github.com>
2023-12-20 16:21:54 +0900

    [Bronze V] Title: 개수 세기, Time: 228 ms, Memory: 17804 KB -BaekjoonHub

---
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    static Scanner sc = new Scanner(System.in);
+    public static int countNum (int[] array, int N) {
+        int count = 0;
+        int v = sc.nextInt();
+
+        for (int i = 0; i < array.length; i++) {
+            if (v == array[i]) {
+                count++;
+            }
+
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+
+        int N = sc.nextInt();
+        int[] array = new int[N];
+
+        for (int i = 0; i < N; i++) array[i] = sc.nextInt();
+        System.out.println(countNum(array, N));
+    }
+}
