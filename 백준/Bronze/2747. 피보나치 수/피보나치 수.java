bd76b73646e0bc4f5515d8c9f38479c922c2f6f6
BuKwon Kim <148305892+bukwon@users.noreply.github.com>
2024-01-08 22:19:00 +0900

    [Bronze II] Title: 피보나치 수, Time: 128 ms, Memory: 14260 KB -BaekjoonHub

---
+++ 백준/Bronze/2747. 피보나치 수/피보나치 수.java
@@ -0,0 +1,18 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(bf.readLine());
+
+        int[] array = new int[n + 1];
+        array[0] = 0; array[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            array[i] = array[i - 1] + array[i - 2];
+        }
+        bw.write(array[n] + "");
+        bw.flush(); bw.close(); bf.close();
+    }
+}
