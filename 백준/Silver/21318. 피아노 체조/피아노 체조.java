d59ce573d9fe466a9cca00418bd6756fc019e7e4
BuKwon Kim <dnu05043@gmail.com>
2024-05-09 20:50:23 +0900

    [Silver I] Title: 피아노 체조, Time: 796 ms, Memory: 63428 KB -BaekjoonHub

---
+++ 백준/Silver/21318. 피아노 체조/피아노 체조.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(bf.readLine());
+        int[] music = new int[100001];
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        for (int i = 1; i <= n; i++) {
+            music[i] = Integer.parseInt(st.nextToken());
+        }
+        int q = Integer.parseInt(bf.readLine());
+        int[] prefix_sum = new int[100001];
+        int tmp = 0;
+        for (int i = 1; i <= n; i++) {
+            if(music[i - 1] > music[i]) tmp++;
+
+            prefix_sum[i] = tmp;
+        }
+        int[] answer = new int[q];
+        int start, end;
+        for (int i = 0; i < q; i++) {
+            String[] num = bf.readLine().split(" ");
+            start = Integer.parseInt(num[0]);
+            end = Integer.parseInt(num[1]);
+            answer[i] = prefix_sum[end] - prefix_sum[start];
+        }
+        for (int i: answer) {
+            bw.write(i + "\n");
+        }
+        bw.flush(); bw.close(); bf.close();
+    }
+}
