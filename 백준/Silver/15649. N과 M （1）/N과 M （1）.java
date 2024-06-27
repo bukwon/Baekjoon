8aacf4b76d49dc2f6898dfa639c6dc3768370b1b
BuKwon Kim <dnu05043@gmail.com>
2024-06-27 23:36:57 +0900

    [Silver III] Title: N과 M (1), Time: 300 ms, Memory: 22828 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Main {
+    static boolean[] visit;
+    public static int[] arr;
+    static BufferedReader bf;
+    static BufferedWriter bw;
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        bf = new BufferedReader(new InputStreamReader(System.in));
+        bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        visit = new boolean[n];
+        arr = new int[m];
+        dfs(n, m, 0);
+        bw.write(sb + "");
+        bw.flush();
+        bf.close();
+        bw.close();
+    }
+
+    private static void dfs(int n, int m, int depth) {   // num1은 배열 체크, num2는 개수 체크
+        if (depth == m) {
+            for (int val: arr) {
+                sb.append(val).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (!visit[i]) {
+                visit[i] = true;
+                arr[depth] = i + 1;
+                dfs(n, m, depth + 1);
+                visit[i] = false;
+            }
+
+        }
+    }
+}
