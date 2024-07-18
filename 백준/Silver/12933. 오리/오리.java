3d040551d23dd0f9b7cd54fc2f7b141a7f2db1e1
BuKwon Kim <dnu05043@gmail.com>
2024-07-18 23:55:20 +0900

    [Silver II] Title: 오리, Time: 188 ms, Memory: 18404 KB -BaekjoonHub

--- 백준/Silver/12933. 오리/오리.java
+++ 백준/Silver/12933. 오리/오리.java
@@ -3,28 +3,30 @@ import java.io.*;
 public class Main {
     static char[] c;
     static int answer = 0;
-    static String s;
+    static int len;
     static char[] cry = {'q', 'u', 'a', 'c', 'k'};
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        s = bf.readLine();
-        c = s.toCharArray();
-        implementation(c);
+        c = bf.readLine().toCharArray();
+        len = c.length;
+        implementation();
     }
 
-    private static void implementation(char[] c) {
-        if (s.length() % 5 != 0) {
+    private static void implementation() {
+        if (len % 5 != 0) {
             System.out.println(-1);
             return;
         }
-        while (!s.isEmpty()) {
+        while (len > 0) {
             int check = 0;  // 오리 울음소리 자리 체크
             boolean flag = false; // 한번도 안울었음을 방지하기 위함
-            for (int i = 0; i < s.length(); i ++) {
+            int tmpLen = len;
+            for (int i = 0; i < tmpLen; i ++) {
                 if (cry[check] == c[i]) {
                     check++;
                     c[i] = ' ';
                     if (check == 5) {
+                        len -= 5;
                         check = 0;
                         flag = true;
                     }
@@ -35,10 +37,10 @@ public class Main {
                 break;
             } else {
                 answer ++;
-                s = String.valueOf(c).replaceAll(" ", "");
+                String s = String.valueOf(c).replaceAll(" ", "");
                 c = s.toCharArray();
             }
         }
-        System.out.println(s.isEmpty() ? answer : -1);
+        System.out.println(len == 0 ? answer : -1);
     }
 }
