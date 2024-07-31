43c4f852581f3c136bf758dc8a88ff57a54e962f
BuKwon Kim <dnu05043@gmail.com>
2024-07-31 22:48:20 +0900

    [Bronze III] Title: 별 찍기 - 5, Time: 388 ms, Memory: 18728 KB -BaekjoonHub

--- 백준/Bronze/2442. 별 찍기 － 5/별 찍기 － 5.java
+++ 백준/Bronze/2442. 별 찍기 － 5/별 찍기 － 5.java
@@ -4,16 +4,15 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
-        int tmp;
-        int star = 1;
-        for (int i = 0; i < N; i++) {
-            tmp = N + i;
-            for (int j = 1; j <= tmp; j++) {
-                if (j > tmp - star) System.out.print("*");
-                else System.out.print(" ");
+        
+        for (int i = 1; i <= N; i++) {
+            for (int j = 0; j < N - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < 2 * i - 1; j++) {
+                System.out.print("*");
             }
             System.out.println();
-            star += 2;
         }
     }
 }
